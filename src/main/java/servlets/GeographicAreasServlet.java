deedcc667f45a95ecc5a0f83961e58c67c313d60
alisondraV <alice.7414122013@gmail.com>
2022-02-23 17:40:42 -0500

    Add needed servlet and jsp

---
+++ src/main/java/servlets/GeographicAreasServlet.java
@@ -0,0 +1,29 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+@WebServlet(name = "GeographicAreasServlet", value = "/geographic-areas-servlet")
+public class GeographicAreasServlet extends HttpServlet {
+    List<String> areasList = Arrays.asList("Canada", "Ontario");
+
+    public List<String> getList() {
+        return areasList;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -2,12 +2,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>JSP - Hello World</title>
+    <title>Group Project 1</title>
 </head>
 <body>
-<h1><%= "Hello World!" %>
-</h1>
-<br/>
-<a href="hello-servlet">Hello Servlet</a>
-</body>
+    <h1>Group Project 1</h1>
+    <h2>Prepared by Alisa Vynohradova and Andrii Denysenko</h2>
+    <br/>
+    <a href="geographicAreas.jsp">Geographic Areas</a>
+    </body>
 </html>
